4e7ebf71fdd3f973cf5a0db495fd6f659b714441
berharju <ber.harju@gmx.com>
2025-03-08 18:43:15 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  double x = 5; // random number between 0 and 10
+  cout << x << endl;
+}
